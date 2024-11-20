2e558d5441270eec615260d73fd50606e6d624dc
Kurt Raschke <kurt@kurtraschke.com>
2024-11-19 22:15:23 -0500

    Remove gcTime setting for react-query which was probably not necessary anyway.

--- src/main.jsx
+++ src/main.jsx
@@ -19,8 +19,7 @@ import "./index.css";
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            refetchOnWindowFocus: false,
-            gcTime: 1000 * 60 * 60 * 24
+            refetchOnWindowFocus: false
         }
     }
 });
